91e9a2643749f8dd42f4f3df48971342266e7b54
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-16 14:50:03 +0100

    Display rooms

--- src/containers/RoomsPage/index.tsx
+++ src/containers/RoomsPage/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Container, Row } from 'reactstrap'
 export class RoomsPage extends React.Component {
+  state = {
+    rooms: [
+      {
+        id: 0,
+        name: "Mała sala konferencyjna"
+      }
+    ]
+  }
   public render() {
-    return "Rooms Page"
+    return (
+      <Container>
+        <Row>
+          {this.state.rooms.map(item => {
+            return item.name
+          })}
+        </Row>
+      </Container>
+    )
   }
 }
 
 export default RoomsPage
+
+
+
+
+
+
+
+
+
+
