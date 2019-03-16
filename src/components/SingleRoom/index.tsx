bb6a068d504b85e05730cb5f6597f92f49b91167
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-16 15:11:17 +0100

    Single Room

---
+++ src/components/SingleRoom/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardBody, CardTitle, Button, Col } from 'reactstrap'
+
+interface SingleRoomsProps {
+  name: string
+}
+
+class SingleRoom extends React.PureComponent<SingleRoomsProps> {
+  public render() {
+    return (
+      <Col md="4">
+        <Card>
+          <CardBody>
+            <CardTitle>{this.props.name}</CardTitle>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </Col>
+    )
+  }
+}
+
+export default SingleRoom
